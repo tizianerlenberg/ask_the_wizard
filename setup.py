abe01100eb5a587081469552912816277f20ab5b
Tizian Erlenberg <mail@erlenberg.net>
2023-12-29 14:30:14 +0100

    ℹ️ Add setup.py, src directory, __init__.py, and main.py
    📦 Added `setup.py` for package configuration.
    📂 Created `src` directory to organize source code.
    📄 Added `__init__.py` inside `impraise` package for Python module.
    🚀 Introduced `main.py` within `impraise` for main functionality.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="impraise",
+    version="0.0.1-a",
+    description="TODO",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tizianerlenberg/impraise",
+    author="Andreas Menzel, Tizian Erlenberg",
+    author_email="mail@andreas-menzel.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+)
---
+++ src/impraise/main.py
@@ -0,0 +1,2 @@
+def hallo():
+    print('hallo welt')
