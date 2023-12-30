e92848ff5c8968ade6eebf665634833486de527e
Tizian Erlenberg <mail@erlenberg.net>
2023-12-30 15:39:43 +0100

    bug fixes

--- src/impraise/pip_importer.py
+++ src/impraise/pip_importer.py
@@ -5,12 +5,21 @@ import subprocess
 
 class PipImporterFinder(importlib.abc.MetaPathFinder):
     def __init__(self):
-        pass
+        self.installing = set()
     def find_spec(self, fullname, path, target=None):
-        print(f'PIP IMPORTER: {fullname}')
-        subprocess.call(["pip", "install", fullname])
-        module = importlib.import_module(fullname)
-        return module
+        if path is None and fullname not in self.installing:
+            package_name = fullname.split('.')[-1]
+            self.installing.add(fullname)
+            print('before installing')
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', package_name])
+            self.installing.remove(fullname)
+            print('importing')
+            module = importlib.import_module(fullname)
+            print('MODULE', module)
+            spec = module.__spec__
+            print('SPEC', spec)
+            return spec
+        return None
     def register(self):
         sys.meta_path.append(self)
     @classmethod
